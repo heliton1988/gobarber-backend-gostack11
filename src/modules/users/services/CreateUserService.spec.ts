fdc2ce29db2c6655982df9e4254e22afb1639258
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-01-04 18:49:12 -0300

    :white_check_mark: test to create a new user

---
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,38 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeUserRepository from '../repositories/fakes/FakeUsersRepository';
+import CreateUserService from './CreateUserService';
+
+describe('CreateUser', () => {
+  it('should be able to create a new user', async () => {
+    const fakeUserRepository = new FakeUserRepository();
+    const createUser = new CreateUserService(fakeUserRepository);
+
+    const user = await createUser.execute({
+      name: 'John Doe',
+      email: 'johndow@exemple.com',
+      password: '123456',
+    });
+
+    expect(user).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new user with same e-mail another', async () => {
+    const fakeUserRepository = new FakeUserRepository();
+    const createUser = new CreateUserService(fakeUserRepository);
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'johndow@exemple.com',
+      password: '123456',
+    });
+
+    expect(
+      createUser.execute({
+        name: 'John Doe',
+        email: 'johndow@exemple.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});

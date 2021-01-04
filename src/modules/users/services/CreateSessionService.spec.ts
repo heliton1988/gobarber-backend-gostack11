7673d1766252fca195406fec60dc2fec48190862
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-01-04 19:44:29 -0300

    :heavy_plus_sign: test user authenticate

---
+++ src/modules/users/services/CreateSessionService.spec.ts
@@ -0,0 +1,37 @@
+// import AppError from '@shared/errors/AppError';
+
+import FakeUserRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
+import CreateSessionService from './CreateSessionService';
+import CreateUserService from './CreateUserService';
+
+describe('AuthenticateUser', () => {
+  it('should be able to authenticate', async () => {
+    const fakeUserRepository = new FakeUserRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUser = new CreateUserService(
+      fakeUserRepository,
+      fakeHashProvider,
+    );
+
+    const createSessionService = new CreateSessionService(
+      fakeUserRepository,
+      fakeHashProvider,
+    );
+
+    const user = await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@exemple.com',
+      password: '123456',
+    });
+
+    const response = await createSessionService.execute({
+      email: 'johndoe@exemple.com',
+      password: '123456',
+    });
+
+    expect(response).toHaveProperty('token');
+    expect(response.user).toEqual(user);
+  });
+});

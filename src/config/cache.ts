23a0269ca65ff3a2aee62b019ad4ac65a692d8fc
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-01-18 17:49:49 -0300

    :sparkles: feat: implements cache to list providers and also to list appointmentts in a specific day

---
+++ src/config/cache.ts
@@ -0,0 +1,21 @@
+import { RedisOptions } from 'ioredis';
+
+interface ICacheConfig {
+  driver: string;
+
+  config: {
+    redis: RedisOptions;
+  };
+}
+
+export default {
+  driver: 'redis',
+
+  config: {
+    redis: {
+      port: 6379, // Redis port
+      host: '127.0.0.1', // Redis host
+      password: undefined,
+    },
+  },
+} as ICacheConfig;

eb96890bfaedaa3da7f8df08fda7cf5fcf7710e3
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-01-21 09:02:18 -0300

    :bug: chore: refactory required function of celebrate for better validation

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -19,7 +19,7 @@ providersRouter.get(
   '/:provider_id/day-availability',
   celebrate({
     [Segments.PARAMS]: {
-      provider_id: Joi.string().uuid().required,
+      provider_id: Joi.string().uuid().required(),
     },
   }),
   providerDayAvailabilityController.index,

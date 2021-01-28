c5e674ccfe5ef8475c97e2302c4ba42a96d29572
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-01-28 17:49:32 -0300

    :recycle: chore: treating the name of image when the user uploaded for the profile

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,6 +1,6 @@
 import multer, { StorageEngine } from 'multer';
 import crypto from 'crypto';
-import path from 'path';
+import path, { extname } from 'path';
 
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
@@ -33,8 +33,8 @@ export default {
     storage: multer.diskStorage({
       destination: tmpFolder,
       filename(request, file, callback) {
-        const fileHash = crypto.randomBytes(10).toString('hex');
-        const fileName = `${fileHash}-${file.originalname}`;
+        const fileHash = crypto.randomBytes(16).toString('hex');
+        const fileName = fileHash + extname(file.originalname);
 
         return callback(null, fileName);
       },
